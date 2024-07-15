1384277f79e09a64aea9d624d16351cc70326537
subitha-r <134250929+subitha-r@users.noreply.github.com>
2024-07-16 00:02:52 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+
+public class MergeSort {
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        // Sizes of the two subarrays
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Temporary arrays to hold the values
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; ++i)
+            leftArray[i] = arr[left + i];
+        for (int j = 0; j < n2; ++j)
+            rightArray[j] = arr[mid + 1 + j];
+
+        // Merge the temporary arrays back into the original array
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray, if any
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray, if any
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+
+            int mid = (left + right) / 2;
+
+            // Recursively sort the first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("\nSorted Array");
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
